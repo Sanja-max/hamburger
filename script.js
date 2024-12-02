95e32faffb938905fb3418a8b45b3c940609a329
Sanja-max <sanjadj80@gmail.com>
2024-12-02 10:28:56 +0100

    ---

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+function myFunction(){
+    var x=document.getElementById("myLinks")
+    if(x.style.display === "block") {
+        x.style.display = "none"
+    }
+    else{
+        x.style.display = "block"
+    }
+}
